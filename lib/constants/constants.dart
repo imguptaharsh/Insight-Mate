93780d9bd71816ac0c5d27375020cbe4edbd1693
imperialrogers <theimperialrogers@gmail.com>
2023-04-30 04:13:11 +0530

    Edited Server Files

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+String uri = 'http://0.0.0.0:3000';
+
 Color scaffoldBackgroundColor = const Color(0xFF343541);
 Color cardColor = const Color(0xFF444654);
 
