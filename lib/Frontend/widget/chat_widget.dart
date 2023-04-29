3c26e98e2dac837ee4b1a94d24d7c9277d9ade27
Aryan Kumar <128920734+09aryan@users.noreply.github.com>
2023-04-29 18:57:45 +0530

    Added share button

--- lib/Frontend/widget/chat_widget.dart
+++ lib/Frontend/widget/chat_widget.dart
@@ -1,54 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:hackathon_gpt/Frontend/widget/text_widget.dart';
+import 'package:hackathon_gpt/widget/text_widget.dart';
+import 'package:whatsapp_share/whatsapp_share.dart';
 
-import '../../Backend/constants/constants.dart';
+import '../constants/constants.dart';
 
-class MyWidget extends StatelessWidget {
+class MyWidget extends StatefulWidget {
   // const MyWidget({super.key});
   final String msg;
   final int chatIndex;
   const MyWidget({super.key, required this.msg, required this.chatIndex});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+Future<void> share() async {
+  await WhatsappShare.share(
+    text: 'Whatsapp share text',
+    linkUrl: 'https://flutter.dev/',
+    phone: '911234567890',
+  );
+}
+
+void sendWhatsapp() {
+  String url = "whatsapp://send?phone=+919650845408&text=${Uri.parse("hello")}";
+  //launchUrl(Uri.parse(url));
+  //launchUrl(Uri.parse(url));
+}
+
+class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Material(
-          color: chatIndex == 0 ? scaffoldBackgroundColor : cardColor,
+          color: widget.chatIndex == 0 ? scaffoldBackgroundColor : cardColor,
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Image.network(
-                  chatIndex == 0
-                      ? 'https://static.vecteezy.com/system/resources/previews/021/608/790/original/chatgpt-logo-chat-gpt-icon-on-black-background-free-vector.jpg'
-                      : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmL_db62k8JH_uZuQi3IWja7VdudVSJAf6fIC7bPadex8GmodDpBFO7MLHpquflN_KS0o&usqp=CAU',
-                  height: 30,
-                  width: 30,
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    // Image.network(
+
+                    widget.chatIndex == 0
+                        ? 'https://static.vecteezy.com/system/resources/previews/021/608/790/original/chatgpt-logo-chat-gpt-icon-on-black-background-free-vector.jpg'
+                        : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmL_db62k8JH_uZuQi3IWja7VdudVSJAf6fIC7bPadex8GmodDpBFO7MLHpquflN_KS0o&usqp=CAU',
+                    // height: 30,
+                    // width: 30,
+                  ),
                 ),
                 const SizedBox(
                   width: 8,
                 ),
-                Expanded(child: TextWidget(label: msg)),
-                chatIndex == 0
+                Expanded(child: TextWidget(label: widget.msg)),
+                widget.chatIndex == 0
                     ? const SizedBox.shrink()
                     : Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         mainAxisSize: MainAxisSize.min,
-                        children: const [
-                          Icon(
-                            Icons.copy,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Icon(
-                            Icons.thumb_down_alt_outlined,
-                            color: Colors.white,
-                          )
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                share();
+                              },
+                              icon: Icon(
+                                Icons.share,
+                                color: Colors.white,
+                              )),
+                          // SizedBox(
+                          //   width: 5,
+                          // ),
                         ],
                       ),
               ],
