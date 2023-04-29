081025cd99ff8ffb417e4e037f5c3c3a8342e176
imperialrogers <theimperialrogers@gmail.com>
2023-04-29 22:34:19 +0530

    Linked Up ChatBot

--- lib/Frontend/widget/chat_widget.dart
+++ lib/Frontend/widget/chat_widget.dart
@@ -1,16 +1,22 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:hackathon_gpt/widget/text_widget.dart';
+import '../../Backend/widgets/text_widget.dart';
 import 'package:whatsapp_share/whatsapp_share.dart';
 
-import '../constants/constants.dart';
+import '../../constants/constants.dart';
 
 class MyWidget extends StatefulWidget {
   // const MyWidget({super.key});
   final String msg;
   final int chatIndex;
-  const MyWidget({super.key, required this.msg, required this.chatIndex});
+  final bool shouldAnimate;
+
+  const MyWidget({
+    Key? key,
+    required this.msg,
+    required this.chatIndex,
+    required this.shouldAnimate,
+  }) : super(key: key);
 
   @override
   State<MyWidget> createState() => _MyWidgetState();
@@ -25,6 +31,7 @@ Future<void> share() async {
 }
 
 void sendWhatsapp() {
+  // ignore: unused_local_variable
   String url = "whatsapp://send?phone=+919650845408&text=${Uri.parse("hello")}";
   //launchUrl(Uri.parse(url));
   //launchUrl(Uri.parse(url));
@@ -56,24 +63,53 @@ class _MyWidgetState extends State<MyWidget> {
                 const SizedBox(
                   width: 8,
                 ),
-                Expanded(child: TextWidget(label: widget.msg)),
+                Expanded(
+                  child: widget.chatIndex == 0
+                      ? TextWidget(
+                          label: widget.msg,
+                        )
+                      : widget.shouldAnimate
+                          ? DefaultTextStyle(
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16),
+                              child: AnimatedTextKit(
+                                  isRepeatingAnimation: false,
+                                  repeatForever: false,
+                                  displayFullTextOnTap: true,
+                                  totalRepeatCount: 1,
+                                  animatedTexts: [
+                                    TyperAnimatedText(
+                                      widget.msg.trim(),
+                                    ),
+                                  ]),
+                            )
+                          : Text(
+                              widget.msg.trim(),
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16),
+                            ),
+                ),
                 widget.chatIndex == 0
                     ? const SizedBox.shrink()
-                    : Row(
+                    : const Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          IconButton(
-                              onPressed: () {
-                                share();
-                              },
-                              icon: Icon(
-                                Icons.share,
-                                color: Colors.white,
-                              )),
-                          // SizedBox(
-                          //   width: 5,
-                          // ),
+                          Icon(
+                            Icons.thumb_up_alt_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Icon(
+                            Icons.thumb_down_alt_outlined,
+                            color: Colors.white,
+                          )
                         ],
                       ),
               ],
