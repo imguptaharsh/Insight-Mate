f509cbd56288d38044b5ed06f78f57c3e3ed3d0a
Harsh Kumar Gupta <111433732+imguptaharsh@users.noreply.github.com>
2023-04-30 11:43:48 +0530

    Image recognization

---
+++ lib/Backend/image_text/lens.dart
@@ -0,0 +1,186 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageLens extends StatefulWidget {
+  const ImageLens({super.key});
+
+  @override
+  State<ImageLens> createState() => _ImageLensState();
+}
+
+class _ImageLensState extends State<ImageLens> {
+  bool textScanning = false;
+  XFile? imageFile;
+
+  String scannedText = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Text Recognition example"),
+      ),
+      body: Center(
+          child: SingleChildScrollView(
+        child: Container(
+            margin: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (textScanning) const CircularProgressIndicator(),
+                if (!textScanning && imageFile == null)
+                  // Container(
+                  //   width: 300,
+                  //   height: 300,
+                  //   color: Colors.grey[300]!,
+                  // ),
+                  if (imageFile != null) Image.file(File(imageFile!.path)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        padding: const EdgeInsets.only(top: 10),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.grey,
+                            backgroundColor: Colors.white,
+                            shadowColor: Colors.grey[400],
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)),
+                          ),
+                          onPressed: () {
+                            getImage(ImageSource.gallery);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 5),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Icons.image,
+                                  size: 30,
+                                ),
+                                Text(
+                                  "Gallery",
+                                  style: TextStyle(
+                                      fontSize: 13, color: Colors.grey[600]),
+                                )
+                              ],
+                            ),
+                          ),
+                        )),
+                    Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        padding: const EdgeInsets.only(top: 10),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.grey,
+                            backgroundColor: Colors.white,
+                            shadowColor: Colors.grey[400],
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)),
+                          ),
+                          onPressed: () {
+                            getImage(ImageSource.camera);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 5),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Icons.camera_alt,
+                                  size: 30,
+                                ),
+                                Text(
+                                  "Camera",
+                                  style: TextStyle(
+                                      fontSize: 13, color: Colors.grey[600]),
+                                )
+                              ],
+                            ),
+                          ),
+                        )),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  scannedText,
+                  style: const TextStyle(fontSize: 20),
+                )
+              ],
+            )),
+      )),
+    );
+  }
+
+  void getImage(ImageSource source) async {
+    try {
+      final pickedImage = await ImagePicker().pickImage(source: source);
+      if (pickedImage != null) {
+        textScanning = true;
+        imageFile = pickedImage;
+        setState(() {});
+        getRecognisedText(pickedImage);
+      }
+    } catch (e) {
+      textScanning = false;
+      imageFile = null;
+      scannedText = "Error occured while scanning";
+      setState(() {});
+    }
+  }
+
+  void getRecognisedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognisedText = await textDetector.processImage(inputImage);
+    await textDetector.close();
+    scannedText = "";
+    for (TextBlock block in recognisedText.blocks) {
+      for (TextLine line in block.lines) {
+        scannedText = "$scannedText${line.text}\n";
+      }
+    }
+    print(scannedText.toString());
+
+    textScanning = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+}
+
+// Widget chooseImageFromCamera() {
+//   return Padding(
+//     padding: const EdgeInsets.only(top: 8, bottom: 8),
+//     child: Container(
+//       height: 55,
+//       decoration: BoxDecoration(
+//         color: Colors.yellow,
+//         borderRadius: BorderRadius.circular(
+//           24,
+//         ),
+//       ),
+//       child: Center(
+//         child: Text(
+//           Constants.chooseImgFromCameraString,
+//           style: TextStyle(color: Colors.black),
+//         ),
+//       ),
+//     ),
+//   );
+// }
